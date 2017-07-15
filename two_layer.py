f5578540a25412c3dfa96e1fb0f350f0e224d57e
Yusuke Saito <ysaito8015@gmail.com>
2017-07-15 16:38:20 +0900

    add propagate function

--- two_layer.py
+++ two_layer.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 
 # define weight
-def weight(dtype = tf.float32, shape = []):
-    initial = tf.trancated_normal(dtype = dtype, shape, stddev = 0.01)
+def weight(shape = []):
+    initial = tf.truncated_normal(shape, stddev = 0.01)
     return tf.Variable(initial)
 
 # define bias
@@ -10,6 +10,9 @@ def bias(dtype = tf.float32, shape = []):
     initial = tf.zeros(shape, dtype = dtype)
     return tf.Variable(initial)
 
+# define sigmoid function
+def sigmoid(x):
+    return (1/(1 + tf.exp(-x)))
 
 # data points
 Q = 5
@@ -20,11 +23,26 @@ R = 1
 X = tf.placeholder(dtype = tf.float32, shape = [None, Q])
 
 # layer 1
+# tf.matmul ( dot product )
+# https://www.tensorflow.org/api_docs/python/tf/matmul
 W1 = weight(shape = [Q, P])
 b1 = bias(shape = [P])
 f1 = tf.matmul(X, W1) + b1
+sigm = sigmoid(f1)
 
 # layer 2
 W2 = weight(shape = [P, R])
 b2 = bias(shape = [R])
-f2 = tf.matmul(f1, W2) + b2
+f2 = tf.matmul(sigm, W2) + b2
+
+# setting Session()
+sess = tf.Session()
+
+# If there is tf.Variables in the code, you should initialize the map.
+init_op = tf.global_variables_initializer()
+sess.run(init_op)
+
+# output
+y = sess.run(f2, {X: data })
+# hidden layer's output
+h = sess.run(sigm, {X: data })
